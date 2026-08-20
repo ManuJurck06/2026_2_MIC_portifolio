eab03cbed36fcd6ca9ed72f73257fbcdba393377
ManuJurck <manuellyjurck.grad@gmail.com>
2026-08-20 11:34:34 -0300

    Experimento com pinos de entrada e chaves.

--- portifolio_mic/aula1_gpio/main.c
+++ portifolio_mic/aula1_gpio/main.c
@@ -10,11 +10,13 @@
 
 int main(void){
 	DDRB |= 0b00100000; // Define PB5 como saída
+	DDRD &= 0b11110111; // Define pino PD3 como entrada --> força o bit 3 ser uma entrada - está zerando ele
     while(1){
-		PORTB |= 0b00100000; // Atribui nível lógico alto ao pino PB5
-		_delay_ms(100);
-		PORTB &= 0b11011111; // Atribui nível lógico baixo ao pino PB5
-		_delay_ms(900);
-		
-    }
+		if((PIND & 0b00001000) != 0) { //Ler valor da chave PD3, segue se tiver em nível lógico alto  --> isolar um bit sempre significa aplicar a operação E -- funciona como um filtro
+			PORTB |= 0b00100000; // Atribui nível lógico alto ao pino PB5
+			_delay_ms(100);
+			PORTB &= 0b11011111; // Atribui nível lógico baixo ao pino PB5
+			_delay_ms(900);
+		}
+    }                                                  
 }
