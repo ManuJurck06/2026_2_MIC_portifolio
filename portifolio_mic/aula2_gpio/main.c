29d67eb83ee7e4f2c92acc7046be5e269895820e
Manuelly Jurck <manuellyjurck.grad@gmail.com>
2026-08-27 09:41:52 -0300

    Versão aula 2.1

--- portifolio_mic/aula2_gpio/main.c
+++ portifolio_mic/aula2_gpio/main.c
@@ -8,22 +8,23 @@
 #define F_CPU 16000000
 #include <xc.h>
 #include "util/delay.h"
+#include "stdint.h"
 
-#define BIT0_MASK 0b00000001 
-#define BIT1_MASK 0b00000010 
-#define BIT2_MASK 0b00000100 
-#define BIT5_MASK 0b00100000 
+//Controle de vários LEDs com operações de shift
 
+void GPIO_config(){
+	DDRB = (1<<DDB6)|(1<<DDB4)|(1<<DDB2)|(1<<DDB0);
+}
 int main(void){
-	//*****Formas diferentes de setar a direção de PB2 e PB5*****
-	//Forma 1: shifts com definições do fabricante
-	DDRB = (1<<DDB5); //Numero 1 escorregado 5 vezes a esquerda 1=100000
-	//DDRB = 0b00100000; -> mesma coisa que a linha acima
-	//Forma 2: Binario Puro
-	//DDRB = 0b00100000 | 0b00000100; = 0b00100100 -> mesma coisa quebrado em duas partes
-	//DDRB = BIT5_MASK | BIT2_MASK;
+	GPIO_config();
+	uint8_t tData = 0x03; //Cria variavel de 8 bits que recebe o numerp hexa 3 -> 0b0000 0011
     while(1)
     {
-        //TODO:: Please write your application code 
+       PORTB = tData;
+	   _delay_ms(1000);
+	  tData = tData <<1; //0b0000 0110, 0b0000 1100
+	  if(tData == 0){
+		  tData = 0x03;
+	  }
     }
 }
