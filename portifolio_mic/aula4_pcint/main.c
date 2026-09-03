f9c633c4489d650496de76db9f38567aff28c3cd
Manuelly Jurck <manuellyjurck.grad@gmail.com>
2026-09-03 09:26:46 -0300

    Adicionada aula sobre PCINT com esqueletos de função

---
+++ portifolio_mic/aula4_pcint/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 9/3/2026 8:57:02 AM
+ *  Author: Aluno
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
