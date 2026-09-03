a97d3365095f752ec0456b53f79b00e2ad87b646
Manuelly Jurck <manuellyjurck.grad@gmail.com>
2026-09-03 08:27:22 -0300

    Envio do cproj faltando aula 3

---
+++ portifolio_mic/aula3_interrupt/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 9/3/2026 8:16:07 AM
+ *  Author: Aluno
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
